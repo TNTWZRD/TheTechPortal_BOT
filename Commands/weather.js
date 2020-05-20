6f345df19153a46a4a39481219510b8cd184fed2
root <root@webserver_pve_lan.lan>
2020-05-20 00:30:27 +0000

    Implemented some APIs

---
+++ Commands/weather.js
@@ -0,0 +1,42 @@
+const Utilities = require('Utilities')
+const fetch = require('node-fetch')
+const querystring = require('querystring')
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'weather',
+    aliases: [],
+    description: 'Get The Weather',
+    help: '!Weather <LOATION>: Get The Weather, US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name',
+    usage: `<LOCATION>`,
+    args: true,
+    guildOnly: true,
+	execute(Bot, msg, _args) {
+        return new Promise(async (resolve, reject) => {
+            const OPTIONS = _args.OPTIONS;
+            const args = _args.ARGS;
+            const USERS = Bot.ServerData.USERS
+
+            const URL = "http://api.weatherapi.com/v1/current.json?key=9fcdb42eb7d4451ca76232839201905&";
+
+            const query = querystring.stringify( { 
+                q:args[0]
+            });
+            var Weather = await fetch(`${URL}${query}`)
+                .then(response => response.json());
+
+            const embedMsg = new Discord.MessageEmbed({
+                title: `Current Weather In ${Weather.location.name} ${Weather.location.tz_id}`,
+                description: `Current Temperature: ${Weather.current.temp_f} F/ ${Weather.current.temp_c} C - ${Weather.current.condition.text}\nWind: ${Weather.current.wind_dir} at ${Weather.current.wind_mph}mph - Pressure: ${Weather.current.pressure_in}in - Precipitation:  ${Weather.current.precip_in}in\nUV: ${Weather.current.uv} - Visibility: ${Weather.current.vis_miles}miles`,
+                footer: {
+                  text: `Last Updated: ${Weather.current.last_updated}`,
+                },
+              })
+              .setThumbnail(Weather.current.condition.icon.replace('//', 'https://'))
+
+            msg.channel.send({embed: embedMsg});
+
+            resolve("!Urban Executed, No Errors");
+        });
+	},
+};
